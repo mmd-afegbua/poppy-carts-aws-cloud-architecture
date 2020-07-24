d42a8878e12d6e8eee19d8bc527cf6bfb7b95989
mmd-afegbua <afegbuaismaila@gmail.com>
2020-07-24 13:29:48 +0100

    added load balancer

---
+++ Terraform/two-tier-iac/asg/web_app_asg/loadbalancer.tf
@@ -0,0 +1,63 @@
+###################################
+# ELB - Application Load Balancer #
+###################################
+
+resource "aws_elb" "asg_elb" {
+  name               = var.elb_name
+  security_groups    = [aws_security_group.elb_sg.id]
+#  availability_zones = var.availability_zones
+  subnets            = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  # This adds a listener for incoming HTTP requests.
+  listener {
+    lb_port           = var.elb_port
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+
+resource "aws_security_group" "elb_sg" {
+  name   = "poppy-carts-external-elb"
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.elb_port
+    to_port     = var.elb_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
+  }
+}
+
+resource "aws_alb_target_group" "poppy_carts_tg" {
+  name     = "poppy-carts-alb-target"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id
+  stickiness {
+    type = "lb_cookie"
+  }
+  # Alter the destination of the health check to be the login page.
+  health_check {
+    path = "/login"
+    port = 80
+  }
+}
